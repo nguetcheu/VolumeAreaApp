0e8f6645f30098a1ae772f885f838e52c0ce920d
nguetcheu <dnguetcheu@gmail.com>
2024-12-04 22:08:44 +0100

    configuration du custom Adapter et création du viewHolder

--- app/src/main/java/com/example/volumeareaapp/MyCustomAdapter.java
+++ app/src/main/java/com/example/volumeareaapp/MyCustomAdapter.java
@@ -1,2 +1,30 @@
-package com.example.volumeareaapp;public class MyCustomAdapter {
+package com.example.volumeareaapp;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+
+// The class will be responsible for creating
+// the view for each item and binding data to it
+public class MyCustomAdapter extends ArrayAdapter<Shape> {
+
+    private ArrayList<Shape> shapeArrayList;
+    private Context context;
+
+    public MyCustomAdapter(@NonNull Context context,ArrayList<Shape> shapeArrayList) {
+        super(context, R.layout.grid_item_layout, shapeArrayList);
+        this.context = context;
+        this.shapeArrayList = shapeArrayList;
+    }
+
+    // View Holder: Used to cache references to the views within an item layout
+    private static class MyViewHolder {
+        TextView shapeName;
+        ImageView shapeImg;
+    }
 }
